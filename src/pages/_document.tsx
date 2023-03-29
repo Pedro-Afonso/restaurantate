1338477b3a111f1f520fdb1a4cb83de57fbf2fad
Pedro-Afonso <pedro.766@hotmail.com>
2023-03-28 22:14:42 -0300

    Initial Setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* start - Google Fonts */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;500;600;700&family=Josefin+Sans:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        {/* end - Google Fonts */}
+      </Head>
       <body>
         <Main />
         <NextScript />
